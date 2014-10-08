97cc4cecda17f619a0123ff17c07832c8bde38c6
bfchu <atuinblue@gmail.com>
2014-10-07 18:47:12 -0700

    adding shiny trails to shiny shoe

--- Unity/Assets/Scripts/ShinyshoeController.cs
+++ Unity/Assets/Scripts/ShinyshoeController.cs
@@ -9,10 +9,15 @@ public class ShinyshoeController : MonoBehaviour {
 	public float musicChange1;
 	public float musicChange2;
 
+	public GameObject shinyTrail1;
+	private float trail1Offset = 0.6f;
+
+
 	private float musicEnd = 65f;
 	private float escapeSpeed = 0.1f;
+	private Quaternion trailsRotations = new Quaternion(90,0,0,90);
 	
-	// Use this for initialization
+
 	void Start () {
 		
 	}
@@ -23,15 +28,22 @@ public class ShinyshoeController : MonoBehaviour {
 		if (Time.time >= musicChange1) {
 			//begin the kicking.
 			transform.position = new Vector3(0.0f,1.0f, Mathf.Sin(Time.time - musicChange1) * amplitude);
+			spawnShinyTrails ();
 		}
+
+
+	}
+
+	void spawnShinyTrails(){
+		Vector3 spawnPosition = new Vector3 (transform.position.x + 1, 0.0f, transform.position.z + trail1Offset);
+		Instantiate(shinyTrail1,spawnPosition,trailsRotations);
 	}
 
 	void escapeSequence(){
 		//The ship slides back towards the center and takes off.
 		transform.position = new Vector3 (transform.position.x - escapeSpeed, 1.0f, transform.position.z * 0.98f);
 	}
-
-	// Update is called once per frame
+	
 	void Update () {
 		if (Time.time < musicEnd) {
 			swingStep ();
---
+++ Unity/Assets/Scripts/ShoeTrailer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShoeTrailer : MonoBehaviour {
+
+	public float trailSpeed = 5;
+	public float lifeTime;
+
+	private float tick;
+	
+	void Start () {
+		rigidbody.velocity = new Vector3 (trailSpeed, 0.0f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tick += Time.deltaTime;
+		if (tick > lifeTime){
+			Destroy (gameObject);
+		}
+
+	}
+}
