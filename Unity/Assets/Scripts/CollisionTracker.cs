6912d157ce1dc66857905368019e8be40b999054
bfchu <atuinblue@gmail.com>
2014-10-07 23:27:46 -0700

    working on getting background to color.lerp, renewed builds

--- Unity/Assets/Scripts/CollisionTracker.cs
+++ Unity/Assets/Scripts/CollisionTracker.cs
@@ -2,21 +2,18 @@
 using System.Collections;
 
 public class CollisionTracker : MonoBehaviour {
-	
 
 	public GameObject currentTarget = null;
 
 	void OnTriggerEnter(Collider other){
 		if (currentTarget == null) {
 			currentTarget = other.gameObject;
-			//Debug.Log("target acquired: " + currentTarget);
 		}
 	}
 
 	void OnTriggerExit(Collider other){
 		if (currentTarget == other.gameObject) {
 			currentTarget = null;
-			//Debug.Log("target left");
 		}
 	}
 
--- Unity/Assets/Scripts/ShinyshoeController.cs
+++ Unity/Assets/Scripts/ShinyshoeController.cs
@@ -9,6 +9,11 @@ public class ShinyshoeController : MonoBehaviour {
 	public float musicChange1; //time stamp of music changes.
 	public float musicChange2;
 
+	public GameObject backdrop;
+	private Color shinyColor = new Color(219,88,0,255);
+	private float sweepDuration = 4;
+	private float sweepTime = 0;
+
 	public GameObject shinyTrail1;
 	public GameObject shinyTrail2;
 	public GameObject shinyTrail3;
@@ -24,12 +29,14 @@ public class ShinyshoeController : MonoBehaviour {
 
 	public GUIText endText1;
 	public GUIText endText2;
+	public GUIText endText3;
 	public GUIText endPrompt;
 	
 
 	void Start () {
 		endText1.text = "";
 		endText2.text = "";
+		endText3.text = "";
 		endPrompt.text = "";
 	}
 
@@ -40,6 +47,14 @@ public class ShinyshoeController : MonoBehaviour {
 			//begin the kicking.
 			transform.position = new Vector3(0.0f,1.0f, Mathf.Sin(shinyTime - musicChange1) * amplitude);
 			spawnShinyTrails ();
+			if(shinyTime >= musicChange2){
+				//not working properly, unsure why.
+				/*sweepTime += Time.deltaTime;
+				float lerp = Mathf.Abs(Mathf.Sin(sweepTime/sweepDuration));
+				backdrop.renderer.material.color = new Color(shinyColor.r *lerp, shinyColor.g *lerp, 0, 255);
+				//Debug.Log("red: " + backdrop.renderer.material.color.r + ", green: " + backdrop.renderer.material.color.g);
+				*/
+			}
 		}
 	}
 
@@ -55,12 +70,14 @@ public class ShinyshoeController : MonoBehaviour {
 	void escapeSequence(){
 		//The ship slides back towards the center and takes off.
 		transform.position = new Vector3 (transform.position.x - escapeSpeed, 1.0f, transform.position.z * 0.98f);
+		backdrop.renderer.material.color = Color.black;
 
 		//After a bit, start displaying the credits.
 		if (shinyTime > musicEnd + creditsDelay) {
 			endText1.text = "Shiny Shoe Starfield";
 			if(shinyTime > musicEnd + creditsDelay + 1){
 				endText2.text = "by Brian Chu";
+				endText3.text = "Music: Airtime by Gus Wellman";
 				if(shinyTime > musicEnd + creditsDelay*2){
 					endPrompt.text = "Press 'R' to replay, press 'Space Bar' to start benchmarking demo.";
 					if(Input.GetKeyDown(KeyCode.R))
