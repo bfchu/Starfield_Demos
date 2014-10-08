57a36c1a7b1f8dad38a1869484a2e83509618a72
bfchu <atuinblue@gmail.com>
2014-10-07 20:55:43 -0700

    added end credit text and replay/mode switch prompt

--- Unity/Assets/Scripts/ShinyshoeController.cs
+++ Unity/Assets/Scripts/ShinyshoeController.cs
@@ -17,20 +17,29 @@ public class ShinyshoeController : MonoBehaviour {
 	private float trail3Offset = -0.6f;
 
 	private float musicEnd = 65f;
+	private float creditsDelay = 2.0f;
 	private float escapeSpeed = 0.1f;
 	private Quaternion trailsRotations = new Quaternion(90,0,0,90);
+	private float shinyTime = 0;
+
+	public GUIText endText1;
+	public GUIText endText2;
+	public GUIText endPrompt;
 	
 
 	void Start () {
-		
+
+		endText1.text = "";
+		endText2.text = "";
+		endPrompt.text = "";
 	}
 
 	void swingStep(){
 		//rotate the shiny shoe back and forth using the sine function and swingPower;
-		transform.rotation = new Quaternion (90, 0, Mathf.Sin (Time.time * swingSpeed)*swingPower, 90);
-		if (Time.time >= musicChange1) {
+		transform.rotation = new Quaternion (90, 0, Mathf.Sin (shinyTime * swingSpeed)*swingPower, 90);
+		if (shinyTime >= musicChange1) {
 			//begin the kicking.
-			transform.position = new Vector3(0.0f,1.0f, Mathf.Sin(Time.time - musicChange1) * amplitude);
+			transform.position = new Vector3(0.0f,1.0f, Mathf.Sin(shinyTime - musicChange1) * amplitude);
 			spawnShinyTrails ();
 		}
 
@@ -49,10 +58,25 @@ public class ShinyshoeController : MonoBehaviour {
 	void escapeSequence(){
 		//The ship slides back towards the center and takes off.
 		transform.position = new Vector3 (transform.position.x - escapeSpeed, 1.0f, transform.position.z * 0.98f);
+		if (shinyTime > musicEnd + creditsDelay) {
+			endText1.text = "Shiny Shoe Starfield";
+			if(shinyTime > musicEnd + creditsDelay + 1){
+				endText2.text = "by Brian Chu";
+				if(shinyTime > musicEnd + creditsDelay*2){
+					endPrompt.text = "Press 'R' to replay, press 'Space Bar' to start benchmarking demo.";
+					if(Input.GetKeyDown(KeyCode.R))
+						Application.LoadLevel(Application.loadedLevel);
+					if(Input.GetKeyDown(KeyCode.Space))
+						Application.LoadLevel("Prefabs_benchmarking");
+				}
+			}
+		}
+
 	}
 	
 	void Update () {
-		if (Time.time < musicEnd) {
+		shinyTime += Time.deltaTime;
+		if (shinyTime < musicEnd) {
 			swingStep ();
 		} else {
 			escapeSequence();
