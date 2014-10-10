015b1f1a9eb1c6d8d3f5b71c48318af2100b6a60
bfchu <atuinblue@gmail.com>
2014-10-10 00:08:51 -0700

    changed star tracking to used x-position instead of 3D collisions, removed unneeded components from star prefab

--- Unity/Assets/Scripts/Star.cs
+++ Unity/Assets/Scripts/Star.cs
@@ -7,17 +7,38 @@ public class Star : MonoBehaviour {
 
 	public float scaleMin;
 	public float scaleMax;
+
+	public int startLine;
+	public int finishLine;
+
+	private float speed;
+	private int speedAdjust = 60;
 	
 	void Start () {
 		//spawn this star with a random velocity, 
-		float speed = Random.Range (minSpeed, maxSpeed);
-		rigidbody.velocity = transform.right * speed;
+		speed = Random.Range (minSpeed, maxSpeed)/speedAdjust;
 		//scale its size by its ratio to maxSpeed.
-		float distance = rigidbody.velocity.x / maxSpeed;
+		float distance = speed * speedAdjust / maxSpeed;
 		float scale = Mathf.Max (scaleMin, scaleMax * distance);
 		transform.localScale *= scale;
+	}
 
 
+	void moveStar(){
+		transform.position = new Vector3(transform.position.x + speed, 0, transform.position.z);
+	}
+
+	void resetPosition(){
+		transform.position = new Vector3 (startLine, 0, transform.position.z);
+	}
+
+
+	void Update(){
+		moveStar ();
+
+		if (transform.position.x > finishLine) {
+			resetPosition();
+		}
 	}
 
 }
